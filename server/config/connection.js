7a42884bc5f5f39c01c9daa98a90b9b006d5df51
BryceedThompson <128741755+BryceedThompson@users.noreply.github.com>
2023-09-20 15:43:23 -0700

    deploy

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,13 +1,6 @@
 const mongoose = require('mongoose');
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false,
-  },
-);
+  process.env.MONGODB_URI || 'mongodb://localhost/booksearch');
 
 module.exports = mongoose.connection;
